11886ab020b30dec3b41ce81ad0b03eb29af40ad
Pejman Samadi <pezhmansamadi21@gmail.com>
2022-09-19 09:29:55 +0000

    improve model with CNN

--- train.py
+++ train.py
@@ -12,7 +12,12 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 
 model = models.Sequential()
 
-model.add(layers.Input((32, 32, 3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
