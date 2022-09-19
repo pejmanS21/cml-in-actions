ba9345792955e03b74e15d92d3a5ddaa8ecbe036
pejmans21 <pezhmansamadi21@gmail.com>
2022-09-19 12:44:02 +0430

    initial commit

---
+++ get_data.py
@@ -0,0 +1,3 @@
+from tensorflow.keras import datasets
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
---
+++ train.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+from tensorflow.keras import layers, models, datasets
+import matplotlib.pyplot as plt
+import json
+
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+model = models.Sequential()
+
+model.add(layers.Input((32, 32, 3)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+with open('model-summary.txt','w') as fh:
+    # Pass the file handle in as a lambda function to make it callable
+    model.summary(print_fn=lambda x: fh.write(x + '\n'))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=5, 
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+
+plt.savefig("results.png")
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+with open("metrics.json", 'w') as outfile:
+    json.dump({"vaildation-accuracy": test_acc, "vaildation-loss": test_loss}, outfile)
